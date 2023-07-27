1900b97695a395fa0cb880e12bdf58afc4c0698b
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-07-27 16:05:01 +0530

    changes from main repository

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
 import "./LoginPage.css";
-import getUsers from "../../userUtils";
+import usersData from "../../user.json";
 import { FIL } from "../../assets/Images";
 import { rightpane } from "../../assets/Images";
 
 const LoginPage = ({ onPageChange }) => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [secret, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    // const users = await getUsers();
-    // const user = users.find((u) => u.email === email && u.password === password);
-    onPageChange("dashboard");
-    // if (user) {
-    //   // Successfully logged in, redirect to the dashboard
-    //   // You can implement the redirect using React Router or any other method
 
-    //   console.log('Logged in successfully!');
-    // } else {
-    //   setError('Invalid credentials');
-    // }
+const handleLogin = (e) => {
+    e.preventDefault();  
+  const user = usersData.users.find(
+    (user) =>
+    
+      user.secret === secret
+  );
+    
+    if (user) {
+      // Successfully logged in, redirect to the dashboard
+      // You can implement the redirect using React Router or any other method
+      onPageChange("dashboard");
+      console.log('Logged in successfully!');
+    } else {
+      setError('Invalid credentials');
+    }
   };
   const handleCreateAccount = () => {
     onPageChange();
@@ -48,7 +52,7 @@ const LoginPage = ({ onPageChange }) => {
               className="input-field"
               type="password"
               placeholder="Secret-Phrase"
-              value={password}
+              value={secret}
               onChange={(e) => setPassword(e.target.value)}
             />
             <button type="submit" className="login-button">
